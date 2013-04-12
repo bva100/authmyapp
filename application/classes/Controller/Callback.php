916c39ee67b9673b0b854c02462ac7a46e8e4c40
Brian Anderson <brianvanderson@gmail.com>
2013-04-11 23:12:15 -0700

    debugging stripe callback

--- application/classes/Controller/Callback.php
+++ application/classes/Controller/Callback.php
@@ -14,7 +14,8 @@ class Controller_Callback extends Controller {
 		Factory_Payment::create('stripe');
 		
 		// use id and send request for stripe event
-		echo Debug::vars($data); die;
+		$event = Stripe_Event::retrieve($data->id);
+		Kohana::$log->add(Log::ERROR, $event->id);
 		
 		// only subscribe to invoice.payment_failed and customer.subscription.deleted
 		// switch ($data->type) {
