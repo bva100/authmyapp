ef82d6e349214cdd04572452dc1fa11db6f2dae5
Brian Anderson <brianvanderson@gmail.com>
2013-04-18 13:54:21 -0700

    updated db config for prod

--- modules/database/config/database.php
+++ modules/database/config/database.php
@@ -1,6 +1,37 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 switch (Kohana::$environment) {
+	case 'prod':
+		return array
+		(
+			'default' => array
+			(
+				'type'       => 'MySQL',
+				'connection' => array(
+					/**
+					 * The following options are available for MySQL:
+					 *
+					 * string   hostname     server hostname, or socket
+					 * string   database     database name
+					 * string   username     database username
+					 * string   password     database password
+					 * boolean  persistent   use persistent connections?
+					 * array    variables    system variables as "key => value" pairs
+					 *
+					 * Ports and sockets may be appended to the hostname.
+					 */
+					'hostname'   => 'localhost',
+					'database'   => 'authmyapp',
+					'username'   => 'root',
+					'password'   => 'fo5Ur_58_baLoo',
+					'persistent' => FALSE,
+				),
+				'table_prefix' => '',
+				'charset'      => 'utf8',
+				'caching'      => FALSE,
+			),
+		);
+		break;
 	case 'dev':
 		return array
 		(
