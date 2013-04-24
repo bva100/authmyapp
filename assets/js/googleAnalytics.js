e9f5dca55c2edfd767916cd2e7acc36e7e2fb338
Brian Anderson <brianvanderson@gmail.com>
2013-04-24 11:55:31 -0700

    debugging analytics

--- assets/js/googleAnalytics.js
+++ assets/js/googleAnalytics.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   
-  ga('create', 'UA-40264055-1', 'https://www.authmyapp.com');
+  ga('create', 'UA-40264055-1', 'www.authmyapp.com');
   ga('send', 'pageview');
